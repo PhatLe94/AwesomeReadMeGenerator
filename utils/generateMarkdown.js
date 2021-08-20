e88f79fed37024dc273a6cc89a7f86c58e0e1214
Phat Le <phatle94@gmail.com>
2021-08-20 16:21:47 -0600

    badges arent working, removed

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -24,11 +24,7 @@ function generateMarkdown(userResponses, userInfo) {
 
   // Generate markdown for the top required portions of the README
   let draftMarkdown = `# ${userResponses.title}
-    ![Badge for GitHub repo top language](https://img.shields.io/github/languages/top/${userResponses.username}/${userResponses.repo}?style=flat&logo=appveyor) ![Badge for GitHub last commit](https://img.shields.io/github/last-commit/${userResponses.username}/${userResponses.repo}?style=flat&logo=appveyor)
-    
-    Check out the badges hosted by [shields.io](https://shields.io/).
-    
-    
+
     ## Description 
     
     *The what, why, and how:* 
